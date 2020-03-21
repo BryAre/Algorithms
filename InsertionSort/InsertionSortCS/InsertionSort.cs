a5a56180bbc63a645ed57e0c76588ddd7c2e6aee
BryAre <37481349+BryAre@users.noreply.github.com>
2020-03-21 11:38:45 -0400

    Insertion Sort using C#

---
+++ InsertionSort/InsertionSortCS/InsertionSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace InsertionSortCSharp
+{
+    class InsertionSort
+    {
+        public static int[] createArray(int[] array, int size){
+
+            for(int i = size; i > 0; i--){
+                array[i-1] = i;
+            }
+            return array;
+        }
+
+        public static void printArray(int[] array, int size){
+            for(int i = 0; i < size; i++){
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        public static int[] sort(int[] array, int size){
+            int i, j ,key;
+
+            for(i = 1; i < size; i++){
+                key = array[i];
+                j = i -1;
+
+                while( j>= 0 && array[j] > key ){
+                    array[j+1] = array[j];
+                    j = j - 1;
+                }
+                array[j+1] = key;
+            }
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            int size = 100;
+            int[] array = new int[size];
+
+            array = createArray(array, size);
+            array = sort(array,size);
+            printArray(array,size);
+
+        }
+    }
+}
